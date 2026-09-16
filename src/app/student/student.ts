16d8f386ad619069acf8e5da8eb5934fb3097113
Nicks008 <n.cerone.3509@vallauri.edu>
2026-09-16 12:54:56 +0200

    Aggiunto background-color

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-student',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
